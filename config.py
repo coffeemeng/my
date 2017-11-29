98a8c5df750cacb07fb9ea351d0871a1df2d92e0
your name <your@email.com>
2017-11-29 16:13:53 +0800

    data

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+'''
+时间:20171127
+作者：恶作剧
+标题：配置
+'''
+DeploymentConfig = {
+    "filehome":"E:/testcode/",
+    "username":"tom"
+}
+TestConfig = {
+    "filehome":"E:/testcode/",  #逗号非常重要
+    "username":"jony"   
+}
---
+++ testrun.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+'''
+时间:20171128
+作者：恶作剧
+备注：计算今天是一年中的第几天
+'''
+from config import DeploymentConfig
+username = DeploymentConfig["username"]
+print(username)
